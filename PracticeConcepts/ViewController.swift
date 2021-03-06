aea5c666d40676c2ed6747fe33056eb2a01225db
emtom <emtom22@gmail.com>
2021-03-06 14:57:37 -0800

    Initial Commit

---
+++ PracticeConcepts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeConcepts
+//
+//  Created by Eric Tom on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
